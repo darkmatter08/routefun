ed0028721f2ac17b4b4e715fef1ddfe76de31d95
Shawn Jain <shawnjain.08@gmail.com>
2014-01-15 18:43:28 -0500

    initial commit, working backend functions with no database

---
+++ routefun/views.py
@@ -0,0 +1,38 @@
+# Create your views here.
+from django.template.loader import get_template
+from django.template import Context
+from django.http import *
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+authors = {'author1' : 'Shawn', 'author2' : 'Andrew'}
+
+# Homepage
+def home(request):
+	t = get_template('construction.html')
+	html = t.render(Context(authors))
+	return HttpResponse(html)
+
+def searchResults(request):
+	if request.method != 'POST':
+		return HttpResponseBadRequest()
+
+	response_data = {}
+	results = []
+	for i in range(4):
+		results.append({ "type" : "home", "number": str(str(i)*10)})
+	response_data["results"] = results
+	return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
+# POST request with route ID to get all GPS Coords
+@csrf_exempt
+def getAllGPSCoords(request):
+	if request.method != 'POST':
+		return HttpResponseBadRequest()
+	routeId = request.POST['routeId']
+
+	response_data = {}
+	response_data['lats'] = [1.23, 4.56, 7.89]
+	response_data['longs'] = [7.89, 4.56, 1.23]
+	return HttpResponse(json.dumps(response_data), content_type="application/json")
