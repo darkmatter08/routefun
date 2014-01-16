8f53d0470df219ea99be2e08b3e3cb2b53091344
Shawn Jain <shawnjain.08@gmail.com>
2014-01-16 11:28:52 -0500

    added lats longs

--- routefun/models.py
+++ routefun/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# class OBD_Trips_enh:
+# 	idVehicle = models.AutoField(primary_key=True)
+# 	idTrip = models.AutoField(primary_key=True)
+# 	ranking = models.Model
+
+# class OBD_CarData:
+# 	idData = 
+# 	idVehicle = models.Model(primary_key=true)
+# 	lat = models.Model()
+# 	lon = models.Model()
